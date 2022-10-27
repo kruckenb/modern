fcdaa41017731f47c1d623129d88b6959a213f97
Pete Kruckenberg <pete@kruckenberg.com>
2022-10-26 21:43:58 -0600

    Translate to Spanish

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from http import HTTPStatus
 
 
 class Handler(http.server.SimpleHTTPRequestHandler):
-    phrase = b'Hello everyone'
+    phrase = b'Hola mundo'
 
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
