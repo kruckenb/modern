db2773bd040210446a6d014dcd18eda0ce355f7a
Pete Kruckenberg <pete@kruckenberg.com>
2022-10-26 21:32:26 -0600

    Initial server

---
+++ server.py
@@ -0,0 +1,16 @@
+import http.server
+import socketserver
+from http import HTTPStatus
+
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    phrase = b'Hello world'
+
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        self.wfile.write(Handler.phrase)
+
+if __name__ == '__main__':
+    httpd = socketserver.TCPServer(('', 8000), Handler)
+    httpd.serve_forever()
